b5417520287229a807069b2159d63dc810ff99bb
prabalp <prabalpanda2008@gmail.com>
2022-01-26 22:31:49 +0530

    start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import "./App.css";
+
+import ImuGraph from "./components/ImuGraph";
+
+function App() {
+  return (
+    <div className="App">
+      haha
+      <ImuGraph />
+    </div>
+  );
+}
+
+export default App;
